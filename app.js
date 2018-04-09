e2a287ccf05deea5b48516b1029bb16490ce51d6
samhughlett <proifone@gmail.com>
2018-04-09 22:05:17 +0000

    added edit function

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require("express"),
     
 app.set("view engine", "ejs");
 mongoose.connect("mongodb://localhost/blog");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname, + 'public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 var blogSchema = new mongoose.Schema({
@@ -17,30 +17,63 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
-// Blog.create({
-//     title: "testing the blog",
-//     image: "https://images.unsplash.com/photo-1474631245212-32dc3c8310c6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ed17683f2c514eed2ab025ff59b1e3b8&auto=format&fit=crop&w=400&q=60",
-//     body: "this is just a test post."
-// });
-
 app.get("/", function(req, res){
     res.redirect("/blogs");
     
 });
 
 app.get("/blogs", function(req, res){
-    Blog.find({}, function(err, blog){
+    Blog.find({}, function(err, blogs){
         if(err){
-            res.render("error");
+            console.log(err)
         }else{
-            res.render("index", {blog: blog});
+            res.render("index", {blogs: blogs});
         }
     });
-    res.render("index");
-    
 });
 
+app.get("/blogs/new", function(req, res){
+    res.render("new")
+})
+// add a blog post
+app.post("/blogs", function(req, res){
+    Blog.create(req.body.blog, function(err, newBlog){
+        if (err){
+            res.redirect("/blogs/new")
+        }else{
+            res.redirect("/blogs")
+            
+        }
+    })
+})
 
+ app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if (err){
+            res.render("blogs")
+        }
+        else{
+            res.render("show", {blog: foundBlog});
+        }
+    })
+ });
+// app.get("/blogs/delete", function(req, res){
+//     res.render("delete")
+// })
+ app.get("/blogs/:id/edit", function(req, res){
+     Blog.findById(req.params.id, function(err, foundBlog){
+         if(err){
+             res.redirect("/blogs")
+         }
+         else{
+             res.render("edit", {blog: foundBlog});
+             
+         }
+     });
+  })
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The blog is now up and runing share your content"); 
 });
+
+
+
